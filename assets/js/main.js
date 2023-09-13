fd07b9cab4d24179476b2407817e7e72ee733bbe
HasanYildz <201118053@samsun.edu.tr>
2023-09-13 20:57:09 +0300

    1.02

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,14 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== SHADOW HEADER ===============*/
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the shadow-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 
 /*=============== EMAIL JS ===============*/
