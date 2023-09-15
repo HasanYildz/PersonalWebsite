4b9493f7ef2db76d37ec87df62f27060a0742d91
HasanYildz <201118053@samsun.edu.tr>
2023-09-16 01:30:17 +0300

    1.09

--- assets/js/main.js
+++ assets/js/main.js
@@ -133,5 +133,10 @@ const sr =ScrollReveal({
     //reset:true
 })
 
-sr.reveal(`.home__perfil`,{origin : 'right'})
-sr.reveal(`.home__name, .home__info`,{origin : 'left'})
+sr.reveal(`.home__perfil, .about__image, .contact__mail`,{origin : 'right'})
+
+sr.reveal(`.home__name, .home__info
+            .about__container, .section__title-1, .about__info,
+            .contact__social, .contact__data`,{origin : 'left'})
+
+sr.reveal(`.services__card, .projects__card`,{interval:100})            
